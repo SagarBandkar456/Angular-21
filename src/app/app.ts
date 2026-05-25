468c3a0a4275a66b2a04a82a2c7a7bd4f3b2204a
sagar.bandkar <sagarbndkr@gmail.com>
2026-05-25 08:29:55 +0530

    init

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { log } from 'console';
+import { Writable } from 'stream';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,101 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+
+  constructor() {
+    effect(() => {
+      console.log('This is value', this.value());
+      if (this.value() == 110) {
+        this.value.set(0)
+      }
+
+      console.log("speed:-", this.speed());
+      if (this.speed() > 0 && this.speed() > 80) this.color = 'green'
+    })
+
+    effect(() => {
+      console.log(this.fruit());
+    })
+  }
+  // Tut 1 interpolation
   protected readonly title = signal('Angular21');
+  name: string = "Sagar Bandkar"
+
+  // Tut 2 interpolcation and function
+  email = "sagarbndkr@gmail.com";
+  getName() {
+    return this.name;
+  }
+
+  //Tut 3 Event binding
+  count = 0;
+  counter(ops: string) {
+    if (ops == 'minus') this.count > 0 && this.count--
+    else this.count++;
+
+    //this.showUserName()
+  }
+
+  showUserName() {
+    alert("Hello")
+  }
+
+  // Tut 4 Imp events
+  handleEvent(event: string) {
+    console.log(event);
+  }
+
+  //Tut 5 Property binding
+  btnDisabled = false;
+  inputReadonly = false;
+
+  toggle() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  // Tut 6 Signals
+  data = 10;
+  value = signal(100);
+
+  updateData() {
+    this.data++;
+  }
+
+  updateValue() {
+    this.value.set(this.value() + 1)
+  }
+
+  height = signal(100);
+  width = signal(20);
+
+  area: Signal<number> = computed<number>(() => {
+    return this.height() * this.width()
+  });
+
+  updateHeight() {
+    this.height.set(this.height() + 10);
+  }
+
+  speed = signal(0);
+  color = "black";
+  fruit = signal('Apple');
+
+  increaseSpeed() {
+    this.speed.set(this.speed() + 10);
+  }
+
+  changeFruit() {
+    this.fruit.set("banana");
+  }
+
+  data1: WritableSignal<string | number | boolean> = signal<string | number | boolean>("sagar");
+  users: WritableSignal<string[]> = signal(['raj', 'sidhu', 'peter']);
+
+  handleData() {
+    this.data1.set("raj");
+    this.users.update((item) => [...item, 'johson'])
+    console.log(this.users());
+
+  }
+
 }
